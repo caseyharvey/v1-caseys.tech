7dc04935a466ca07d06c98dc4adcd1b40ccef373
caseyharvey <caseyharvey1@gmail.com>
2020-03-09 19:22:24 +1100

    Refactor sass. Add about me

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -11,20 +11,31 @@ class HomePage extends React.Component {
           <div className='name'>Casey Harvey</div>
           <div className='title'>Software Developer</div>
         </div>
-        <div className='about-flex-box'>
+        <div className='about-container'>
           <div className='about-me'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa error
-            corrupti enim debitis dolorem magni labore sapiente deserunt
-            tenetur, facilis expedita earum iusto beatae quis, fuga, amet
-            explicabo ipsam id nisi repellendus itaque. Totam atque quas
-            officiis dicta voluptatem facilis delectus nemo molestias dolor
-            aliquam, fugit perspiciatis repellendus quo pariatur sint voluptas
-            ut dolore quaerat, nihil corrupti? Cum in atque quam ipsa similique
-            dolor, odio exercitationem laboriosam repudiandae quasi numquam
-            asperiores ullam saepe inventore assumenda sit, voluptatibus
-            aspernatur non eum voluptas! In tenetur asperiores reiciendis
-            consectetur voluptatum? Fuga, nihil esse libero hic praesentium
-            perspiciatis architecto deleniti! Iusto ex exercitationem sunt!
+            <p>
+              I really love developing software. The whole process is deeply
+              satisfying and never ending. There is always something new to
+              learn, some improvement to be made and something just out of
+              reach. I get rush from solving problems and leveling up my skills.
+              This keeps me motivated and inspired to push through the
+              inevitable challenges along the way.
+            </p>
+            <p>
+              Writing code and seeing it come to life is a very rewarding
+              process. The reward is so valuable because its so difficult to do
+              well. I am hooked on the process and not the end result. I focus
+              on having solid habits that improve with every win and every loss.
+              these habits ensure that I always deliver my best. And that I'm
+              willing to smash it all to pieces and build it again.
+            </p>
+            <p>
+              Thanks to the internet and all the amazing people producing
+              valuable content I have taught myself how to code. Since making
+              the decision to learn a programming language my life has been
+              forever changed and I could not imagine myself doing anything
+              else.
+            </p>
           </div>
         </div>
       </div>
