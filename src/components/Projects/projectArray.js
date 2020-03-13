134e12038fe872195439d62c5059d99ef7860dbd
caseyharvey <caseyharvey1@gmail.com>
2020-03-14 10:45:08 +1100

    Update urls

--- src/components/Projects/projectArray.js
+++ src/components/Projects/projectArray.js
@@ -3,7 +3,7 @@ export const projectArray = [
     id: 1,
     name: 'Time is money',
     type: 'react redux redux-form',
-    url: 'https://timeismoney.caseyharvey.space/',
+    url: 'https://timeismoney.caseys.tech/',
     github: 'https://github.com/caseyharvey/time-is-money/tree/master/src',
     about:
       "I built this app as a way to keep track of time while I study. We all know how precious time is but we don't always use it for things that matter. This app can help you clarify whats important in your life by giving it a dollar value."
@@ -12,7 +12,7 @@ export const projectArray = [
     id: 2,
     name: 'umbleJay',
     type: 'react react-hook-form',
-    url: 'https://umblejay.caseyharvey.space/',
+    url: 'https://umblejay.caseys.tech/',
     github: 'https://github.com/caseyharvey/umbleJay/tree/master/src',
     about:
       'This is pig latin converter. After seeing many people attempt to program this idea without truly succeeding, I wrote an algorithm to handle the conversion properly. The first attempt was done a while ago and I have since refactored it and turned in into a react app.'
