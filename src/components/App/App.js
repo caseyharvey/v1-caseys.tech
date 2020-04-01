d4c5a03480030d9b170ca4324b63dafb5da6de2a
caseyharvey <caseyharvey1@gmail.com>
2020-04-02 09:12:49 +1100

    remove footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-// import PrismSnippet from '../Prism/PrismSnippet';
-import ProjectList from '../Projects/ProjectList';
-import HomePage from '../HomePage/HomePage';
-import Contact from '../Contact/Contact';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Navbar/Footer';
-import './app.sass';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import ProjectList from "../Projects/ProjectList";
+import HomePage from "../HomePage/HomePage";
+import Contact from "../Contact/Contact";
+import Navbar from "../Navbar/Navbar";
+import "./app.sass";
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Navbar />
-        <div className='main-container'>
-          <Route path='/' exact component={HomePage} />
-          <Route path='/Projects' exact component={ProjectList} />
-          <Route path='/Contact' exact component={Contact} />
+        <div className="main-container">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/Projects" exact component={ProjectList} />
+          <Route path="/Contact" exact component={Contact} />
         </div>
-        <Footer />
       </BrowserRouter>
     );
   }
