4278141f609f44418dc34f9c5ef6c4b368fb999d
Casey Harvey <caseyharvey@caseys-mbp.home>
2021-02-25 13:07:05 +1100

    removed blog link and adjusted nav layout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './navbar.sass';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbar.sass";
 
 const Navbar = () => {
   return (
-    <nav className='navbar'>
-      <div className='nav-container'>
-        <Link to='/' className='about'>
-          about
-        </Link>
-        <div className='right-side'>
-          <Link to='/Projects'>projects</Link>
-          <Link to='/Contact'>contact</Link>
-          <a
-            target='_blank'
-            rel='noopener noreferrer'
-            href='https://blog.caseys.tech/'
-          >
-            blog
-          </a>
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="right-side">
+          <Link to="/" className="about">
+            about
+          </Link>
+          <Link to="/Projects">projects</Link>
+          <Link to="/Contact">contact</Link>
         </div>
       </div>
     </nav>
